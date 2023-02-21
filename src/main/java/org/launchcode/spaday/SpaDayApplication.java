b1a3904b00efed7798054b28ab1e97d85cbdf325
Amy Stockhausen <stockhausenamy@gmail.com>
2023-02-20 20:42:23 -0600

    Finished studio

--- src/main/java/org/launchcode/spaday/SpaDayApplication.java
+++ src/main/java/org/launchcode/spaday/SpaDayApplication.java
@@ -2,8 +2,10 @@ package org.launchcode.spaday;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@RequestMapping("user")
 public class SpaDayApplication {
 
 	public static void main(String[] args) {
